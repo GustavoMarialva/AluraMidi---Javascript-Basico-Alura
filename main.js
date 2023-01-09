cd326bf27c6ef4a7dc85b24566893519e8cdae53
Gustavo Marialva <gmarialva@gmail.com>
2023-01-09 11:24:40 -0300

    adiciona const tecla, instrumento e idAudio

--- main.js
+++ main.js
@@ -1,8 +1,25 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play();
+// function tocaSomPom() {
+//   document.querySelector("#som_tecla_pom").play();
+// }
+
+function tocaSom(idElementoAudio) {
+  document.querySelector(idElementoAudio).play();
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
+let contador = 0;
+
+while (contador < listaDeTeclas.length) {
+  const tecla = listaDeTeclas[contador];
+  const instrumento = tecla.classList[1];
+
+  // template string ${instrumento}
+  const idAudio = `#som_${instrumento}`;
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
+  contador += 1;
+}
 // function tocaSomClap() {
 //   document.querySelector("#som_tecla_clap").play();
 // }
