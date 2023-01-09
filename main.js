0c073dc2c42850b92a1fdf00877b8ce3268a5bc2
Gustavo Marialva <gmarialva@gmail.com>
2023-01-09 12:01:03 -0300

    adiciona for

--- main.js
+++ main.js
@@ -7,19 +7,15 @@ function tocaSom(idElementoAudio) {
 }
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
   const instrumento = tecla.classList[1];
-
-  // template string ${instrumento}
-  const idAudio = `#som_${instrumento}`;
+  const idAudio = `#som_${instrumento}`; // template string ${instrumento}
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-  contador += 1;
 }
+
 // function tocaSomClap() {
 //   document.querySelector("#som_tecla_clap").play();
 // }
