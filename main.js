818c88f9d42bb141efa6d60ed7eed106cf5463f7
Gustavo Marialva <gmarialva@gmail.com>
2023-01-09 10:52:28 -0300

    adiciona querySelectorAll listadeTeclas e const

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 function tocaSomPom() {
   document.querySelector("#som_tecla_pom").play();
 }
+const listaDeTeclas = document.querySelectorAll(".tecla");
 
-function tocaSomClap() {
-  document.querySelector("#som_tecla_clap").play();
-}
+// function tocaSomClap() {
+//   document.querySelector("#som_tecla_clap").play();
+// }
 
-document.querySelector(".tecla_pom").onclick = tocaSomPom; //tocaSomPom esta sem () para que a função possa ser guardada no onclick
+// document.querySelector(".tecla_pom").onclick = tocaSomPom; //tocaSomPom esta sem () para que a função possa ser guardada no onclick
